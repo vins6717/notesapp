b8a1ec29a04466fa35ece290d55cfa74b4d87375
Shannon Gurule <shannon.gurule@gmail.com>
2022-07-31 22:41:02 -0400

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NotesApp from './components/NotesApp';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-      <BrowserRouter>
-        <NotesApp />
-      </BrowserRouter>
+     
+        <HashRouter>
+          <NotesApp />
+        </HashRouter>
+      
    
   </React.StrictMode>
 );
